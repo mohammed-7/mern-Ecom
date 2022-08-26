7c4dc3a09650a2cefcffedc1db57644c26002a10
mohammed-7 <khanhuzaif@live.com>
2022-08-27 00:59:54 +0530

    git commit -a -m"new-one"

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Ecommerce</a>
+      </header>
+      <main>List products.</main>
+    </div>
+  );
+}
+
+export default App;
