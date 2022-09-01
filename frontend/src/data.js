358c88068a37d382b699123068e46e8749303a0f
mohammed-7 <khanhuzaif@live.com>
2022-09-01 12:13:14 +0530

    added product images

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //dimensions of images are 679px * 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      descriptiion: 'high quality shirt',
+    },
+    {
+      name: 'Addidas Fit shirt',
+      slug: 'addidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Addidas',
+      rating: 4.0,
+      numReviews: 10,
+      descriptiion: 'high quality Product',
+    },
+    {
+      name: 'Nike Slim pant',
+      slug: 'Nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 250,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      descriptiion: 'high quality Product',
+    },
+    {
+      name: 'Addidas Fit pant',
+      slug: 'Addidas-Fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      descriptiion: 'high quality Product',
+    },
+  ],
+};
+
+export default data;
