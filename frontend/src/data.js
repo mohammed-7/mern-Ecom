80873c8eda48bd32e2c7bbad393302598dcbb83d
mohammed-7 <khanhuzaif@live.com>
2022-09-05 13:14:05 +0530

    backend engage

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,7 +1,7 @@
 const data = {
   products: [
     {
-      name: 'Nike',
+      name: 'Nike slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/p1.jpg', //dimensions of images are 679px * 829px
